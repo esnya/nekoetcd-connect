6601b942b53ecc851dbc3f411accff69b6001b02
ukatama <dev.ukatama@gmail.com>
2016-02-24 17:38:11 +0900

    Add spec

--- rspec/container_spec.rb
+++ rspec/container_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe service('node') do
-  it { should be_running }
+    it { should be_running }
+end
+
+describe command('docker exec ${ETCD_CONTAINER_ID} /etcdctl get ${APP_NAME}') do
+    it { should eq `docker inspect --format="{{.NetworkSettings.IPAddress}}" ${APP_NAME}` }
 end
