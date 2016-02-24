727916f90da686f6835e48bd116b3efb6a7779d5
ukatama <dev.ukatama@gmail.com>
2016-02-24 18:15:41 +0900

    Fix spec

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
-describe service('node') do
-    it { should be_running }
+describe command('docker ps | grep %s' % [ENV['TARGET_CONTAINER_ID']]) do
+    its(:exit_status) { should eq 0 }
+    its(:stderr) { should eq '' }
 end
 
-describe command('docker exec ${ETCD_CONTAINER_ID} /etcdctl get ${APP_NAME}') do
-    it { should eq `docker inspect --format="{{.NetworkSettings.IPAddress}}" ${APP_NAME}` }
+describe command('docker exec %s /etcdctl get %s' % [ENV['ETCD_CONTAINER_ID'], ENV['APP_NAME']]) do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should eq `docker inspect --format="{{.NetworkSettings.IPAddress}}" ${APP_CONTAINER_ID}` }
+    its(:stderr) { should eq '' }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
 require 'serverspec'
 
-set :backend, :docker
-set :docker_url, ENV['DOCKER_HOST']
-set :docker_container, ENV['TARGET_CONTAINER_ID']
+set :backend, :exec
